f96fda40a43d8320fdc347e0f9b97b029e7ac090
wince1910 <gabrielwince.otavio@gmail.com>
2025-10-01 13:16:00 -0300

    feat: added contato page and finished the main app tsx as App.tsx

--- ConecteCare/src/App.tsx
+++ ConecteCare/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import QuemSomos from './pages/QuemSomos';
+import FAQ from './pages/FAQ';
+import Contato from './pages/Contato';
+import Teleconsulta from './pages/Teleconsulta'; 
+import MenuCuidador from './pages/MenuCuidador';
+import CadastroCuidador from './pages/CadastroCuidador'; 
+import GuiaUsuario from './pages/GuiaUsuario';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/guia-usuario" element={<GuiaUsuario/>} />
+        <Route path="/quem-somos" element={<QuemSomos />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/cadastro-cuidador" element={<CadastroCuidador />} />
+        <Route path="/teleconsulta" element={<Teleconsulta />} /> 
+        <Route path="/menu-cuidador" element={<MenuCuidador />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
